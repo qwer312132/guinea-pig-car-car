abfaceb6f2557df8a5d44fcc1449d2cdd8b7dbe6
jennychen90 <84784606+jennychen90@users.noreply.github.com>
2025-04-17 13:25:18 +0800

    Create GuineaPig.java
    GuineaPig.java

---
+++ GuineaPig.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GuineaPig {
+    private String name;
+    private List<String> foodList;
+
+    public GuineaPig(String name) {
+        this.name = name;
+        this.foodList = new ArrayList<>();
+    }
+
+    public String noise() {
+        return "PUI PUI";
+    }
+
+    public void eat(String food) {
+        foodList.add(food);
+    }
+
+    public String pupu() {
+        String food = String.join(", ", foodList);
+        foodList.clear();
+        return name + ": 棒出了 " + food;
+    }
+
+    public String checkAndPupuIfNeeded() {
+        if (foodList.size() > 5) {
+            return pupu();
+        }
+        return "";
+    }
+
+    public String getName() {
+        return name;
+    }
+}
