a93415c2ffb396ff34189fff4655b70d27be0116
Charlie <86840060+CharlieLai0405@users.noreply.github.com>
2025-04-17 15:01:17 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+import models.GuineaPigCarCar;
+import models.RunRunGuineaPigCarCarRace;
+import models.Car.CarType.CARTYPE;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        GuineaPigCarCar shiromo = new GuineaPigCarCar("西羅摩", CARTYPE.POLICECAR);
+        GuineaPigCarCar abbey = new GuineaPigCarCar("阿比", CARTYPE.AMBULANCE);
+        GuineaPigCarCar teddy = new GuineaPigCarCar("泰迪", CARTYPE.TRASHTRUCK);
+
+
+        ArrayList<GuineaPigCarCar> contestants = new ArrayList<>();
+        contestants.add(shiromo);
+        contestants.add(abbey);
+        contestants.add(teddy);
+
+
+        RunRunGuineaPigCarCarRace race = new RunRunGuineaPigCarCarRace(contestants);
+        race.run_race();
     }
 }
