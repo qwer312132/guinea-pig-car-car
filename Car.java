01167b433103fcad4b9fd2e5c1747ca2e143986d
葉皓允 <ryanyeh818@gmail.com>
2025-04-17 13:42:06 +0800

    Create Car.java

---
+++ Car.java
@@ -0,0 +1,28 @@
+package Car;
+
+public class Car {
+    private int initialSpeed;
+    private int speed;
+    private int numAccelerate;
+
+    public Car() {
+        this(0, 0);
+    }
+
+    public Car(int initialSpeed, int speed) {
+        this.initialSpeed = initialSpeed;
+        this.speed = speed;
+    }
+
+    public int getSpeed() {
+        return this.speed;
+    }
+
+    public void accelerate() {
+        this.speed += this.numAccelerate;
+    }
+
+    public void resetSpeed() {
+        this.speed = this.initialSpeed;
+    }
+}
